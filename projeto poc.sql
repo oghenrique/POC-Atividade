625ad1002b1bdbce9a70909a2a95f299d1559d93
Nicolas Petri <123608034+nvpetri@users.noreply.github.com>
2023-10-20 14:23:04 -0300

    Update projeto poc.sql

--- projeto poc.sql
+++ projeto poc.sql
@@ -113,3 +113,27 @@ create table atividades_extras (
     
     foreign key (id_turma) references turmas(id)
 );
+
+insert into empresas (nome, cnpj, tipo_parceria, duracao_parceria) 
+values
+("Senai", 76095658000196, "Estágios", 2)
+("YouTube", 09622667000109, "Projetos", 4)
+("Google", 22044876000172, "Estágios", 8)
+("Fiap", 61554609000105, "Projetos", 3)
+
+
+INSERT INTO professores (nome,cpf,telefone,data_nascimento,data_contratacao,endereco,email)
+VALUES
+("Tanisha Harrell","129442227800","(882) 247-1794","Jan 1, 2020","Jun 30, 2024","Ap #776-4126 Dolor St.","tanishaharrell@gmail.com"),
+("Dante Guerra","008651524679","(474) 574-8063","Sep 14, 2019","Dec 26, 2023","192 Dis Ave","danteguerra@gmail.com"),
+("Yvette Gibson","407218467402","(942) 557-9560","Jan 8, 2023","Aug 4, 2023","660-8184 Quis, Ave","yvettegibson@gmail.com"),
+("Joelle Salinas","645075378740","(551) 856-4453","Oct 13, 2023","May 3, 2024","495 Mattis. Road","joellesalinas1901@gmail.com"),
+("Cynthia Carson","352151036886","(496) 464-7684","Jan 10, 2024","Nov 24, 2022","Ap #554-3308 Vestibulum Street","cynthiacarson8755@gmail.com");
+
+INSERT INTO alunos (nome,cpf,telefone,data_nascimento,endereco,email)
+VALUES
+("Jackson Luna","155645198335","(569) 476-8670","Feb 26, 2024","Ap #513-9001 Sem Rd.","jacksonluna9185@gmail.com"),
+("May Barker","366946617872","(123) 574-2512","Jan 27, 2024","Ap #297-9276 Nunc Avenue","maybarker4949@gmail.com"),
+("Eleanor Harrison","536554914771","(836) 273-1426","Sep 27, 2018","3361 Sed Av.","eleanorharrison6928@gmail.com"),
+("Quon Hardy","653652872821","(245) 599-8199","Dec 12, 2022","Ap #523-6817 A Rd.","quonhardy@gmail.com"),
+("Molly Good","679688721389","(435) 553-8441","May 7, 2020","9635 Quis St.","mollygood9938@gmail.com");
